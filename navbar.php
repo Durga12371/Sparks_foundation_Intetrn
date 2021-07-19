47ae8d0871c26eb1695b3a6908a5320e77bc9f3b
Durga12371 <82439279+Durga12371@users.noreply.github.com>
2021-07-19 17:13:29 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">RGUKT BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color:Magenta"> Home </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"  style="color:Magenta">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"  style="color:Magenta">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"  style="color:Magenta">Transaction History</a>
+              </li>
+          </div>
+       </nav>
